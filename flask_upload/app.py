1ae05048256c710f6b3ba7e51e1d31ab121d25c0
Arif Yousuf <arifyousuf22@gmail.com>
2020-12-08 01:02:07 +0530

    CORS ORigin added

--- flask_upload/app.py
+++ flask_upload/app.py
@@ -1,22 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,make_response,jsonify
 from werkzeug.utils import secure_filename
+from flask_cors import CORS, cross_origin
+
 app = Flask(__name__)
+# CORS(app, support_credentials=True)
+# cors=CORS(app,resources={
+#     r"/*":{
+#         "origins":"*"
+#     }
+# })
+# app.config['CORS_HEADERS'] = 'Content-Type'
 
-# @app.route('/upload')
-# def upload_file():
-#     return render_template('upload.html')
-
-@app.route('/')
-def upload_file():
-    return render_template('upload.html')
 
 @app.route('/uploader', methods = ['GET', 'POST'])
+@cross_origin(origin='*')
 def uploader_file():
-    
     if request.method == 'POST':
         f = request.files['file']
+        print(f.filename)
         f.save(secure_filename(f.filename))
-        return 'file uploaded successfully'
+        response = make_response(jsonify({"message": 'file uploaded'}))
+        return  response 
+
 		
 if __name__ == '__main__':
    app.run(debug = True)
